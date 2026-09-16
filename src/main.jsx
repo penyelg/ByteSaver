5304537c7347fdd41140881e76e1c16fe9bff8f8
Peniel Garcia <penielmorcia@gmail.com>
2026-09-16 11:48:40 +0800

    favicon

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,14 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import faviconUrl from './assets/bytesaver-mark-transparent.png'
 import './styles/global.css'
+
+const favicon = document.querySelector('link[rel="icon"]') || document.createElement('link')
+favicon.rel = 'icon'
+favicon.type = 'image/png'
+favicon.href = faviconUrl
+document.head.appendChild(favicon)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
